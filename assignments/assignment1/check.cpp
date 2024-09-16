072c1a00dbbe833be1c34eac52a04d19d8b2df39
sadhika huria <sadhikahuria@gmail.com>
2024-09-16 11:23:50 -0700

    commit

--- assignments/assignment1/check.cpp
+++ assignments/assignment1/check.cpp
@@ -1,20 +1,24 @@
-#include "Song.h"
-#include "PlayList.h"
 #include <iostream>
-#include <sstream>
 
 using namespace std;
 
-int main(){
-    PlayList myplaylist;
-    Song song1("hello", "whtever", 234);
-    Song song2("ok", "idc", 234);
-    myplaylist.insert(song1, 0);
-    myplaylist.insert(song2, 1);
-    Song get1 = myplaylist.get(0);
-    Song get2 = myplaylist.get(1);
-
-    cout << myplaylist.size() << endl;
-
-    return 0;
+int foos(int arr[], int i)
+{
+    if (i == 0) {
+        return arr[i];
+    }
+    else {
+        return arr[i] + foos(arr, i - 1);
+    }
 }
+
+int main(){
+    
+
+int arr[] = { 1,2,7,3,4,3 };
+cout << foos(arr, 5) << endl;
+
+
+}
+
+
