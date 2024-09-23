df3d206e5b089bd4f3a853c22163a6fccbfd093e
sadhika huria <sadhikahuria@gmail.com>
2024-09-22 20:22:18 -0700

    committ

--- Lectures/important.cpp
+++ Lectures/important.cpp
@@ -1,9 +1,45 @@
 /*
     Things to search:
-        Implement a stack
+        Implement a stack (with linked list)
         Implement a queue
+            Priority queues
+            ordered maps
+            ordered sets
+            Trees
+            intro to graph
         Implement binary search history
         Implement balanced binary search history
         Implement a hashtable
+            containers
         
+        Smart pointers
+        
+        Sorting 
+        https://www.cs.usfca.edu/~galles/visualization/ComparisonSort.html
+            selection sort
+            insertion sort
+            bubble sort
+            quick sort
+            merge sort
+            heap sort 
+
+        writing a class in cpp
+
+        specfic aspects
+            Exception handling
+            operator overloading
+            templates
+        
+*/
+
+/* 
+    Notes
+        Trees
+            rooted at the top
+            one less edge than vertices
+                
+        Hashtable
+            insert items mapped to specific indexes (no traversing)
+
+                       
 */
