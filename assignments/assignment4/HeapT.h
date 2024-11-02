4de02b18ef72b27fbbcb77df942cb2496bc2ad57
sadhika huria <sadhikahuria@gmail.com>
2024-11-02 01:20:34 -0700

    co

--- assignments/assignment4/HeapT.h
+++ assignments/assignment4/HeapT.h
@@ -7,28 +7,51 @@
 does size refer to the size allocated by the array, or num
 of items in the array
 */
-
+template <typename T>
 
 class HeapT
 {
     private:
-        int *arr{nullptr};
+        T *arr{nullptr};
         unsigned int capacity{};
         unsigned int count{};
-        int *root{arr};
+        const int *root{arr};
         void bubbleup(int index);
         void bubbledown(int index);
 
     public:
-        HeapT(int val): arr{new int{val}}, capacity{val} {};
-        int min(){return *root;}
-        ~HeapT();
+        HeapT(int val): arr{new T{val}}, capacity{val} {};
+
+        T min(){ 
+            return *root;
+        }
+
+        ~HeapT(){
+            delete[] arr;
+            arr = nullptr;
+            root = nullptr;
+        }
+
         /*overload assignment operator
             deep copies, deallocates dynamic memory 
             */
-        void insert(int);
-        int remove();   // throw a runtime error
-        int peek();     // throw a runtime error
+        
+        void insert(T newval){
+            if(count < capacity){
+                arr[count]={newval};
+                bubbleup(count);
+                count++;
+            }
+        }
+
+        T remove(){
+            T temp = *root;
+            arr[0] = arr[count-1];
+            count--;
+            bubbledown(0);
+        }   // throw a runtime error
+        
+        T peek();     // throw a runtime error
         HeapT& merge(const HeapT&);
         int size();
 
