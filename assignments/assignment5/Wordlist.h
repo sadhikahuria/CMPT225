601dbfeff9331159e03d642976fd5451a8ed07d9
sadhika.huria <sadhikahuria@gmail.com>
2024-11-19 14:05:00 -0800

    commit

--- assignments/assignment5/Wordlist.h
+++ assignments/assignment5/Wordlist.h
@@ -12,11 +12,19 @@ public:
 	// parent - pointer to parent
 	// left - pointer to left child
 	// right - pointer to right child
+    AVLTreeNode *parent{nullptr};
+    AVLTreeNode *left{nullptr};
+    AVLTreeNode *right{nullptr};
 	// word - node's string data
 	// count - number of times the word appears
 	// height - node's height
+    unsigned int word{};
+    unsigned int count{};
+    unsigned int height{};
 
 	// Constructors ...
+    AVLTreeNode(){};
+
 };
 
 // Wordlist class
@@ -24,12 +32,27 @@ class Wordlist
 {
 private:
 	// Class attributes go here
+
 	// Your class MUST have a root node named root (below)
 	AVLTreeNode* root; // DO NOT REMOVE
 
 public:
 	// public methods go here
-
+    Wordlist();
+    Wordlist(string file1);
+    Wordlist(Wordlist &list);
+    Wordlist& operator=(const Wordlist &other);
+    ~Wordlist();
+    unsigned getCount();
+    bool contains(string word);
+    void insert();
+    bool remove(string word);
+    unsigned int differentWords();
+    unsigned int totalwords();
+    string mostFrequent();
+    unsigned int singletons();
+    void printWords();
+    
 	// Prints useful statistics about the word list
 	void printStatistics() const;
 
